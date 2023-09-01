165521de4e476e83419406f1a650ee5dd3339952
RAIFFTECH <raifftechsas@gmail.com>
2023-08-31 23:00:43 -0500

    Actualiza  Geinner

--- ModelosBase.py
+++ ModelosBase.py
@@ -9,23 +9,28 @@ from django.core.exceptions import ValidationError
 def validate_numeric(value):
     if not re.match(r'^[0-9]+$', value):
         raise ValidationError('El número de celular debe contener solo dígitos numéricos.')
+    
 OPC_BOOL = [('S','Si'),('N','No')]
+
 OPC_CLASEDOC  = (
-        ('C', 'Cedula'),
+        ('C', 'Cédula de Ciudadanía'),
         ('T', 'Tarjeta de Identidad'),
         ('N', 'Nit'),
         ('R', 'Registro Civil'),
-        ('E', 'Cedula de Extranjeria'),
+        ('E', 'Cédula de Extranjería'),
+        ('P', 'Pasaporte'),
         ('O', 'Otros'),
     )
+
 OPC_REGIMEN  = (
-        ('Sin Regimen', 'Sin Regimen'),
-        ('Simplificado', 'Simplificado'),
+        ('48', 'Responsable'),
+        ('99', 'No Responsable'),
         ('Comun', 'Comun'),
     )
+
 OPC_TIPTER  = (
-        ('N', 'Natural'),
-        ('J', 'Juridico'),
+        ('N', 'Persona Natural'),
+        ('J', 'Persona Jurídica'),
         ('O', 'Otro'),
     )
 
@@ -33,7 +38,8 @@ OPC_PRODUCTO  = (
         ('AP', 'Aportes'),
         ('AH', 'Ahorros'),
         ('CR', 'Creditos'),
-        ('CC', 'Ctas X Pagar'),
+        ('CC', 'Cuenta por Cobrar'),
+        ('CP', 'Cuenta por Pagar'),
         ('CO', 'Contable'),
         ('BA', 'Bancos'),
     )
@@ -42,8 +48,10 @@ OPC_CANALES  = (
         ('ATM', 'Red de Cajeros'),
         ('POS', 'Compras en Comercios'),
         ('IVR', 'Audio Respuesta'),
-        ('CON', 'C'),
+        ('TRA', 'Transferencia'),
         ('CON', 'Consignacion'),
+        ('EFE', 'Efectivo'),
+        ('CHE', 'Cheque'),
         ('GIR', 'Giro'),
         ('WEB', 'Portal Transaccional'),
         ('MOV', 'Banca Móvil'),
@@ -53,74 +61,76 @@ OPC_CANALES  = (
     )
 
 OPC_NAT  = (
-        ('D', 'Debito'),
-        ('C','Credito'),
+        ('D', 'Débito'),
+        ('C', 'Crédito'),
     )
 
 OPC_TERMINO  = (
         ('D', 'Definido'),
-        ('I','Indefinido'),
+        ('I', 'Indefinido'),
     )
 
 OPC_PER_LIQ_INT  = (
         ('D', 'Diario'),
-        ('M','Mensual'),
-        ('C','Cdat')
+        ('M', 'Mensual'),
+        ('C', 'Cdat'),
+        ('V', 'Vencimiento'),
     )
 
 OPC_EST_CTA_AHO  = (
         ('A', 'Activa'),
-        ('I','Inactiva'),
-        ('C','Cancelada'),
-        ('E','Embargada')
+        ('I', 'Inactiva'),
+        ('C', 'Cancelada'),
+        ('E', 'Embargada'),
     )
 
 OPC_SEXO = (
         ('M', 'Masculino'),
-        ('F','Femenino'),
-        ('N','No Aplica')
+        ('F', 'Femenino'),
+        ('N', 'No Aplica'),
     )
 
 OPC_LIQ_INT_AHO  = (
-        ('C', 'Causacion Final'),
-        ('M','Causacion Mensual'),
-        ('P','Pago')
+        ('C', 'Causación Final'),
+        ('D', 'Causación Diaria'),
+        ('M', 'Causación Mensual'),
+        ('V', 'Causación Vencimiento'),
+        ('P', 'Pago'),
     )
 
 OPC_CRE_TERMINO  = (
         ('D', 'Definido'),
-        ('C','Cupo'),
-        ('R','Rotativo')
+        ('C', 'Cupo'),
+        ('R', 'Rotativo'),
     )
 
 OPC_CRE_FOR_PAG  = (
         ('P', 'Personal'),
-        ('L','Libranza'),
+        ('L', 'Libranza'),
     )
 
 OPC_CRE_EST_JUR = (
         ('N', 'Normal'),
-        ('P','Persuasivo'),
-        ('J','Juridico'),
-        ('C','Condonacion'),
-        ('T','Costigado'),
+        ('P', 'Persuasivo'),
+        ('J', 'Cobro Jurídico'),
+        ('C', 'Condonación'),
+        ('T', 'Castigado'),
     )
 
 OPC_CRE_CATEGORIA = (
-        ('A','Normal'),
-        ('B','Apreciable'),
-        ('C','En Peligro'),
-        ('D','En Mora'),
-        ('E','Irrecuperable'),
-        ('F','Castigado'),
+        ('A', 'Normal'),
+        ('B', 'Apreciable'),
+        ('C', 'En Peligro'),
+        ('D', 'En Mora'),
+        ('E', 'Irrecuperable'),
+        ('F', 'Castigado'),
     )
 
-
 OPC_EST_CRE = (
-        ('X','Por Causar'),
-        ('A','Activo'),
-        ('C','Cancelado'),
-        ('T','Terminado Cupo'),
+        ('X', 'Por Causar'),
+        ('A', 'Activo'),
+        ('C', 'Cancelado'),
+        ('T', 'Terminado Cupo'),
     )
 
 class CLIENTES(models.Model):
