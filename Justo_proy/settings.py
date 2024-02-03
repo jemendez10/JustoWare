8bbc16196458ef04590a92adb3c39da10b9225d8
Jesse <52003010+jemendez10@users.noreply.github.com>
2024-02-03 11:33:56 -0500

    update static

--- Justo_proy/settings.py
+++ Justo_proy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,19 +81,19 @@ WSGI_APPLICATION = 'Justo_proy.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'justoware_BD',
-        'USER': 'root',
-        'PASSWORD': 'MariaDB2023',
-        'HOST': 'localhost',
-        'PORT': '2023',
-
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'justoware_BD',
+    #     'USER': 'root',
+    #     'PASSWORD': 'MariaDB2023',
+    #     'HOST': 'localhost',
+    #     'PORT': '2023',
+
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
@@ -131,6 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'assets')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
